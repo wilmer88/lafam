c6585a2a9977c3511fadabaa38b220b97df8a23b
Wilmer Rivera <wilmerbaby@gmail.com>
2023-02-19 02:47:28 -0500

    styled main page

--- client/src/app/usersList/usersList.component.ts
+++ client/src/app/usersList/usersList.component.ts
@@ -1,36 +1,19 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, isStandalone, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Ifammembers } from './Imembers';
 import { UserService } from './user.service';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
-@Component({
+ @Component({
+  
   selector: 'coat-users',
   templateUrl: './usersList.component.html',
   styleUrls: ['./userList.component.css']
 })
-export class UsersTable implements OnDestroy {
-  jfammembers:any[]= [
-    // {
-    //    "ID":1,
-    // "Firstname": "felix",
-    // "Happiness": 5,
-    // "Urlstr": "assets/images/wilmer.jpg"
-    // },
+export class UsersTable implements OnInit {
 
-    // {
-    //     "ID":2,
-    //  "Firstname": "doris",
-    //  "Happiness": 4,
-    //  "Urlstr": "assets/images/wilmer.jpg"
-    //  },
 
-    //  {
-    //     "ID":3,
-    //  "Firstname": "wilmer",
-    //  "Happiness": 2,
-    //  "Urlstr": "assets/images/wilmer.jpg"
-    //  },
-]
 
 
   cardTitle: string = "Family Members";
@@ -39,28 +22,51 @@ export class UsersTable implements OnDestroy {
   showImage = false;
   errorMessage: string = '';
   sub!: Subscription;
+  // listFilters1: string = 'cart';
+
   private _listFilter: string = '';
 
   get listFilter(): string {
     return this._listFilter;
-  }
+  };
 
   set listFilter(value: string) {
     this._listFilter = value;
     console.log("in setter: ", value);
+    // this.filterMembers = this.performFilter(value);
     this.filterMembers = this.performFilter(value);
-  }
+  };
 
   filterMembers: Ifammembers[] = [];
-  fammembers: Ifammembers[] = [];
+  jfammembers:Ifammembers[]= [
+    {
+       "ID":1,
+    "Firstname": "felix",
+    "Happiness": 5,
+    "Urlstr": "assets/images/wilmer.jpg"
+    },
 
-  constructor(private userService: UserService) { }
+    {
+        "ID":2,
+     "Firstname": "doris",
+     "Happiness": 4,
+     "Urlstr": "assets/images/wilmer.jpg"
+     },
+
+     {
+        "ID":3,
+     "Firstname": "wilmer",
+     "Happiness": 2,
+     "Urlstr": "assets/images/wilmer.jpg"
+     },
+]
+
+  // constructor(private userService: UserService) { }
 
   performFilter(filterBy: string): Ifammembers[] {
     filterBy = filterBy.toLowerCase();
-    return this.fammembers.filter((member: Ifammembers) =>
-      member.Firstname.toLocaleLowerCase().includes(filterBy)
-    );
+    return this.jfammembers.filter((member: Ifammembers) =>
+      member.Firstname.toLocaleLowerCase().includes(filterBy));
   }
 
   // fam: string = "members";
@@ -69,21 +75,24 @@ export class UsersTable implements OnDestroy {
     this.showImage = !this.showImage;
   }
 
-  // ngOnInit(): void {
-  //   this.sub = this.userService.getUsers().subscribe({
-  //     next: (mem) => {
-  //       this.fammembers = mem;
-  //       this.filterMembers = this.fammembers;
-  //     },
-  //     error: (err) => (this.errorMessage = err),
-  //   });
-  // }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
+  ngOnInit(): void {
+    console.log("In OnInit")
+    this.listFilter=""
+    // this.sub = this.userService.getUsers().subscribe({
+    //   next: (mem) => {
+    //     this.fammembers = mem;
+    //     this.filterMembers = this.fammembers;
+    //   },
+    //   error: (err) => (this.errorMessage = err),
+    // });
   }
+
+  // ngOnDestroy(): void {
+  //   this.sub.unsubscribe();
+  // }
 
   onRatingClicked(message: string): void {
     this.cardTitle = "product list: " + message;
   }
+
 }
