fd024daf48980c32e1807e8ba67750c762396bce
Wilmer Rivera <wilmerbaby@gmail.com>
2023-02-21 08:41:30 -0500

    heroku build pack edition 1.3.0

--- client/server.js
+++ client/server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/client'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+'/index.html'));});
 app.listen(process.env.PORT || 4200);
