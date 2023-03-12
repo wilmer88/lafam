15760904b579cc9ca21132540db3dc3fb86098cf
wilmerbaby <wilmerbaby@gmail.com>
2023-03-12 19:07:30 -0400

    heroku set up

--- client/src/app/usersList/user.service.ts
+++ client/src/app/usersList/user.service.ts
@@ -9,7 +9,7 @@ import { Ifammembers } from './Imembers';
 export class UserService {
 
   // private userUrl = 'http://localhost:8080/lafamily';
-  private userUrl = 'http://localhost:8080/lafamily/';
+  private userUrl = '/lafamily';
 
  
   constructor(private http: HttpClient) { };
