056e1746c4220bc23489432a154a2ef7535a176f
wilmerbaby <wilmerbaby@gmail.com>
2023-03-12 20:20:20 -0400

    added public folder with static angular buld

--- main.go
+++ main.go
@@ -1,76 +1,46 @@
 package main
 
 import (
-	// "embed"
-
 	"os"
 
-	// "crypto/tls"
-	// "net/http"
-
-	// "github.com/gin-contrib/cors"
-	// "github.com/gin-contrib/static"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/wilmer88/lafam/controllers"
 )
 
-
-
 func main() {
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-	//     w.Write([]byte("Hello, TLS!"))
-	// })
-
-	// http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil)
-	// http.Handle("http://localhost:4200/lafamily", http.FileServer(http.Dir("./client/dist/client")))
-	// http.Handle("https://mifamily-app.herokuapp.com", http.FileServer(http.Dir("./public/client/dist")))
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-
-
-	// r.Static( "/public", "./client/dist" )
-
-	// r := gin.Default()
-	// r.StaticFS("fs", http.FileSystem(http.FS(f)))
-	// r.GET("localhost:8080/lafamily", func(c *gin.Context) {
-	// 	c.File("./public/dist/client/index.html")
-	// })
 	r := setupRouter()
 	_ = r.Run(":" + port)
-	// tlsConfig := &tls.Config{
-	// 	MinVersion: tls.VersionTLS12,
-	// }
 
-	// server := &http.Server{
-	// 	Addr:      ":8080",
-	// 	TLSConfig: tlsConfig,
-	// 	Handler:   r,
-	// }
-
-	// if err := server.ListenAndServeTLS("path/to/cert.pem", "path/to/key.pem"); err != nil {
-	// 	panic(err)
-	// }
 }
 
 func setupRouter() *gin.Engine {
-	r := gin.Default();
-	r.Static("/", "./public");
-	config := cors.DefaultConfig();
-	config.AllowOrigins = []string{"https://mifamily-app.herokuapp.com"}
-	// config.AllowOrigins = []string{"http://localhost:8080/lafamily"};
-	r.Use(cors.New(config));
-	userRepo := controllers.New();
-	r.POST("/lafamily", userRepo.CreateUser);
-	r.GET("/lafamily/:id", userRepo.GetUser);
-	r.PUT("/lafamily/:id", userRepo.UpdateUser);
-	r.DELETE("/lafamily/:id", userRepo.DeleteUser);
-	// r.Use(static.Serve("https://mifamily-app.herokuapp.com/lafamily", static.LocalFile("./public/client",true)))
+    gin.SetMode(gin.DebugMode)
+    r := gin.Default()
 
-	return r;
+    // Set up CORS
+    config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"https://mifamily-app.herokuapp.com"}
+    // config.AllowOrigins = []string{"http://localhost:8080/lafamily"}
+    r.Use(cors.New(config))
+
+    // Set up static file serving
+    static := r.Group("/static")
+    static.Static("/", "./public")
+
+    // Set up dynamic routes
+    userRepo := controllers.New()
+    r.POST("/lafamily", userRepo.CreateUser)
+    r.GET("/lafamily", userRepo.GetUser)
+    r.GET("/lafamily/user/:id", userRepo.GetUser)
+    r.PUT("/lafamily/user/:id", userRepo.UpdateUser)
+    r.DELETE("/lafamily/user/:id", userRepo.DeleteUser)
+
+    return r
 }
