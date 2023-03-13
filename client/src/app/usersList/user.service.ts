5f122e2a18c5d6cb1db5c0f8b930ba5cc62b3835
wilmerbaby <wilmerbaby@gmail.com>
2023-03-13 01:20:13 -0400

    heroku deploy test

--- client/src/app/usersList/user.service.ts
+++ client/src/app/usersList/user.service.ts
@@ -8,8 +8,8 @@ import { Ifammembers } from './Imembers';
 
 export class UserService {
 
-  private userUrl = 'http://localhost:8080/lafamily';
-  // private userUrl = '/lafamily';
+  // private userUrl = 'http://localhost:8080/lafamily';
+  private userUrl = '/lafamily';
   // private userUrl = 'https://localhost:4200/lafamily';
 
   
