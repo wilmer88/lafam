b0f683d68fdc47c8997094052a16cab9496cd638
wilmerbaby <wilmerbaby@gmail.com>
2023-04-07 02:15:31 -0400

    angular package.json file setup

--- client/server.js
+++ client/server.js
@@ -1,8 +1,15 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/client'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.use(requireHTTPS);
+app.use(express.static("./dist/client"));
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/client'}
+);
+});
