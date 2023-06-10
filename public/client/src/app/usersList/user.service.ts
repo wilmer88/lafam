d8864dd2b6d940f1f36189281da32e7c3a0f0495
wilmerbaby <wilmerbaby@gmail.com>
2023-06-10 09:04:52 -0400

    changed url

--- public/client/src/app/usersList/user.service.ts
+++ public/client/src/app/usersList/user.service.ts
@@ -7,7 +7,7 @@ import { Ifammembers } from './Imembers';
 })
 
 export class UserService {
-  private userUrl = "/";
+  private userUrl = "https://mifamily-app.herokuapp.com";
   // private userUrl = "http://localhost:5100";
 
   constructor(private http: HttpClient) { };
